48a2286e6c3846803c8a168b9b11cdbc008ce698
Ciara Bujanos <c.buj22@gmail.com>
2017-08-24 13:11:44 -0600

    add person class

---
+++ user.js
@@ -0,0 +1,7 @@
+export default class User {
+  constructor({ name, currentPosition, destination }) {
+    this.name = name;
+    this.currentPosition = currentPosition;
+    this.destination = destination;
+  }
+}
